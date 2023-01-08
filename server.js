9096540d9001e2de1d12ea4a8d207d529b415807
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-01-07 20:19:09 -0600

    switched to apollo server and added graphql tools

--- server.js
+++ server.js
@@ -1,79 +1,33 @@
 const express = require("express");
-const { buildSchema } = require("graphql");
-const { graphqlHTTP } = require("express-graphql");
+const { ApolloServer } = require("apollo-server-express");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { loadFilesSync } = require("@graphql-tools/load-files");
 
-const schema = buildSchema(`#graphql
-  type Query{
-    products: [Product]
-    orders: [Order]
-  }
-
-  type Product{
-    id: ID!
-    description: String!
-    price: Float!
-    reviews: [Review]
-  }
-
-  type Review{
-    rating: Int!
-    comment: String
-  }
-
-  type Order{
-    date: String!
-    subtotal: Float!
-    items: [OrderItem]
-  }
-
-  type OrderItem{
-    product: Product!
-    quantity: Int!
-  }
-`);
-
-const root = {
-  products: [
-    {
-      id: "redshow",
-      description: "Red Shoe",
-      price: 42.12,
-    },
-    {
-      id: "bluejean",
-      description: "Blue Jean",
-      price: 55.55,
-    },
-  ],
-
-  orders: [
-    {
-      date: "2005-05-05",
-      subtotal: 90.22,
-      items: [
-        {
-          product: {
-            id: "redshoe",
-            description: "Old Red Shoe",
-            price: 45.11,
-          },
-          quantity: 2,
-        },
-      ],
-    },
-  ],
-};
-
-const app = express();
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
-
-app.listen(3000, () => {
-  console.log("Running GraphQL server...");
+const typesArray = loadFilesSync("**/*", {
+  extensions: ["graphql"],
 });
+
+const resolversArray = loadFilesSync("**/*", {
+  extensions: ["resolvers.js"],
+});
+
+async function startApolloServer() {
+  const app = express();
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+  });
+
+  const server = new ApolloServer({
+    schema: schema,
+  });
+
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(3000, () => {
+    console.log("Running GraphQL server...");
+  });
+}
+
+startApolloServer();
